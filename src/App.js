901377bf848296491b921c0f909eea87e8e632be
VI <ilmar.vodi@voco.ee>
2025-01-10 10:46:12 +0200

    added year filtering functionality

--- src/App.js
+++ src/App.js
@@ -6,13 +6,19 @@ import NewExpense from './comp/NewExpense/NewExpense';
 const DUMMY_EXPENSES = [
   {
     id: Math.random().toString(),
-    date: new Date(2024, 10, 22),
+    date: new Date(2024, 10, 21),
+    title: 'New Toy',
+    price: 12.99
+  },
+  {
+    id: Math.random().toString(),
+    date: new Date(2024, 4, 22),
     title: 'New Book',
     price: 39.99
   },
   {
     id: Math.random().toString(),
-    date: new Date(2024, 10, 22),
+    date: new Date(2023, 10, 14),
     title: 'New Jeans',
     price: 99.99
   }
--- src/comp/Expenses/Expenses.js
+++ src/comp/Expenses/Expenses.js
@@ -1,18 +1,24 @@
 import './Expenses.css'
+import { useState } from 'react'
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from './ExpensesFilter';
 
 const Expenses = (props) => {
+    const [displayYear, setDisplayYear] = useState(new Date().getFullYear().toString())
     const yearChangeHandler = (selectedYear) => {
-        console.log(`${selectedYear} in expenses`)
+        setDisplayYear(selectedYear)
     }
+
+    const filteredExpenses = props.expenses.filter((expense) => {
+        return expense.date.getFullYear().toString() === displayYear
+    })
 
     return (
         <Card className="expenses">
-            <ExpensesFilter onChangeYear={yearChangeHandler}/>
+            <ExpensesFilter selected={displayYear} onChangeYear={yearChangeHandler}/>
             {
-            props.expenses.map((expense) => {
+            filteredExpenses.map((expense) => {
                 return <ExpenseItem data={expense} key={expense.id}/>
             })
             }
